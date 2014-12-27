6e088878357e50625bc57bb00c6449a69d3daeec
Behnam Ahmad khan beigi <yottanami@gmail.com>
2014-12-27 23:58:55 +0330

    Update grep_list_in_dir.rb

--- grep_list_in_dir.rb
+++ grep_list_in_dir.rb
@@ -1,1 +1,15 @@
 #/usr/bin/env ruby
+
+puts "Enter file path that contains list files"
+file = gets.chomp
+puts "Enter the url that should be check"
+url = gets.chop + "/*"
+
+files = Dir[url]
+
+File.open(file).each do  |line|
+  line.gsub!(/\n/, '')
+  if  files.grep(/#{line}/).length < 1
+      puts "#{line}"
+  end
+end
