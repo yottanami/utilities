ce6e44108a28d19e43b405581b740014aef19b31
Behnam Ahmad khan beigi <yottanami@gmail.com>
2015-02-26 22:59:04 +0330

    Update grep_list_in_dir.rb

--- grep_list_in_dir.rb
+++ grep_list_in_dir.rb
@@ -1,5 +1,6 @@
 #/usr/bin/env ruby
-
+# This simple script grep list of files in a specific 
+# path from a text file
 puts "Enter file path that contains list files"
 file = gets.chomp
 puts "Enter the url that should be check"
